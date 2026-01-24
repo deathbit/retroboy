d98a058fb5ba84b2bf3c7b327e6a13f262bc193a
Zhang Jian <deathbit007@gmail.com>
2026-01-25 03:20:55 +0800

    fix

---
+++ src/main/java/com/github/deathbit/retroboy/RetroboyApplication.java
@@ -0,0 +1,13 @@
+package com.github.deathbit.retroboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroboyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroboyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/deathbit/retroboy/RetroboyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.deathbit.retroboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroboyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
