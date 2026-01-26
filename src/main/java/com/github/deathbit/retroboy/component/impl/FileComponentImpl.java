8c354755b7f3708c3279792c7f196adc14a8bb63
jzhang52 <jzhang52@ebay.com>
2026-01-26 11:03:06 +0800

    fix

--- src/main/java/com/github/deathbit/retroboy/component/impl/FileComponentImpl.java
+++ src/main/java/com/github/deathbit/retroboy/component/impl/FileComponentImpl.java
@@ -20,12 +20,6 @@ import java.util.stream.Stream;
 @Component
 public class FileComponentImpl implements FileComponent {
 
-    private final ProgressBarComponent progressBarComponent;
-
-    public FileComponentImpl(ProgressBarComponent progressBarComponent) {
-        this.progressBarComponent = progressBarComponent;
-    }
-
     @Override
     public void deleteFile(String filePath) throws IOException {
         Files.delete(Paths.get(filePath));
@@ -57,32 +51,23 @@ public class FileComponentImpl implements FileComponent {
 
     @Override
     public void batchDeleteFile(List<String> filePaths) throws IOException {
-        progressBarComponent.start("Batch Delete Files", filePaths.size());
         for (String filePath : filePaths) {
             deleteFile(filePath);
-            progressBarComponent.update("Deleted file: " + filePath);
         }
-        progressBarComponent.finish();
     }
 
     @Override
     public void batchDeleteDir(List<String> dirPaths) throws IOException {
-        progressBarComponent.start("Batch Delete Directories", dirPaths.size());
         for (String dirPath : dirPaths) {
             deleteDir(dirPath);
-            progressBarComponent.update("Deleted directory: " + dirPath);
         }
-        progressBarComponent.finish();
     }
 
     @Override
     public void batchDeleteDirContent(List<String> dirPaths) throws IOException {
-        progressBarComponent.start("Batch Delete Directory Contents", dirPaths.size());
         for (String dirPath : dirPaths) {
             deleteDirContent(dirPath);
-            progressBarComponent.update("Deleted directory content: " + dirPath);
         }
-        progressBarComponent.finish();
     }
 
     @Override
@@ -92,12 +77,9 @@ public class FileComponentImpl implements FileComponent {
 
     @Override
     public void batchCreateDir(List<String> dirPaths) throws IOException {
-        progressBarComponent.start("Batch Create Directories", dirPaths.size());
         for (String dirPath : dirPaths) {
             createDir(dirPath);
-            progressBarComponent.update("Created directory: " + dirPath);
         }
-        progressBarComponent.finish();
     }
 
     @Override
@@ -114,9 +96,9 @@ public class FileComponentImpl implements FileComponent {
         Path srcPath = Paths.get(copyDirInput.getSrcDirPath());
         Path destPath = Paths.get(copyDirInput.getDestDirPath());
         Path targetPath = destPath.resolve(srcPath.getFileName());
-        
+
         Files.createDirectories(targetPath);
-        
+
         try (Stream<Path> walk = Files.walk(srcPath)) {
             List<Path> sources = walk.toList();
             for (Path source : sources) {
@@ -134,9 +116,9 @@ public class FileComponentImpl implements FileComponent {
     public void copyDirContent(CopyDirContentInput copyDirContentInput) throws IOException {
         Path srcPath = Paths.get(copyDirContentInput.getSrcDirPath());
         Path destPath = Paths.get(copyDirContentInput.getDestDirPath());
-        
+
         Files.createDirectories(destPath);
-        
+
         try (Stream<Path> walk = Files.walk(srcPath)) {
             List<Path> sources = walk.filter(source -> !source.equals(srcPath)).toList();
             for (Path source : sources) {
@@ -152,32 +134,23 @@ public class FileComponentImpl implements FileComponent {
 
     @Override
     public void batchCopyFile(List<CopyFileInput> copyFileInputs) throws IOException {
-        progressBarComponent.start("Batch Copy Files", copyFileInputs.size());
         for (CopyFileInput copyFileInput : copyFileInputs) {
             copyFile(copyFileInput);
-            progressBarComponent.update("Copied file: " + copyFileInput.getSrcFilePath());
         }
-        progressBarComponent.finish();
     }
 
     @Override
     public void batchCopyDir(List<CopyDirInput> copyDirInputs) throws IOException {
-        progressBarComponent.start("Batch Copy Directories", copyDirInputs.size());
         for (CopyDirInput copyDirInput : copyDirInputs) {
             copyDir(copyDirInput);
-            progressBarComponent.update("Copied directory: " + copyDirInput.getSrcDirPath());
         }
-        progressBarComponent.finish();
     }
 
     @Override
     public void batchCopyDirContent(List<CopyDirContentInput> copyDirContentInputs) throws IOException {
-        progressBarComponent.start("Batch Copy Directory Contents", copyDirContentInputs.size());
         for (CopyDirContentInput copyDirContentInput : copyDirContentInputs) {
             copyDirContent(copyDirContentInput);
-            progressBarComponent.update("Copied directory content: " + copyDirContentInput.getSrcDirPath());
         }
-        progressBarComponent.finish();
     }
 
     @Override
@@ -193,11 +166,8 @@ public class FileComponentImpl implements FileComponent {
 
     @Override
     public void batchRenameFile(List<RenameFileInput> renameFileInputs) throws IOException {
-        progressBarComponent.start("Batch Rename Files", renameFileInputs.size());
         for (RenameFileInput renameFileInput : renameFileInputs) {
             renameFile(renameFileInput);
-            progressBarComponent.update("Renamed file: " + renameFileInput.getSrcFilePath());
         }
-        progressBarComponent.finish();
     }
 }
